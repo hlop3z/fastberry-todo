6923ab342882366bfb36da8e5bfdcc467382e6db
hlop3z <23062270+hlop3z@users.noreply.github.com>
2022-11-28 16:41:14 -0600

    Update todo.py

--- apps/todo/graphql/todo.py
+++ apps/todo/graphql/todo.py
@@ -24,24 +24,24 @@ class Todo:
         """Query"""
 
         async def search(status: str | None = None) -> fb.edges(types.Task):
-            """(Search-Task) Read the Docs"""
+            """Search for task(s) by status"""
             return await manager.Task.search(status)
 
         async def detail(item: fb.ID) -> fb.query(types.Task):
-            """(Detail-Task) Read the Docs"""
+            """Get task by ID"""
             return await manager.Task.detail(item)
 
     class Mutation:
         """Mutation"""
 
         async def create(form: forms.Task) -> fb.mutation(types.Task):
-            """(Create-Task) Read the Docs"""
+            """Create a new task"""
             return await manager.Task.create(form.input)
 
         async def update(item: fb.ID, status: str) -> fb.mutation(types.Task):
-            """(Update-Task) Read the Docs"""
+            """Change task's status"""
             return await manager.Task.update(item, status)
 
         async def delete(item: fb.ID) -> bool:
-            """(Delete-Task) Read the Docs"""
+            """Delete task by ID"""
             return await manager.Task.delete(item)
